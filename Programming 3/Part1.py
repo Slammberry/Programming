152a541478d561bdb152e07126882764a7d5ee46
Slammberry <1714404@brunel.ac.uk>
2018-11-16 16:15:16 +0000

    New thing

---
+++ Programming 3/Part1.py
@@ -0,0 +1,6 @@
+my_dict = {'item1': 4, 'item2': 5, 'item3': 6}
+a = {}
+for i,v in my_dict.items():
+    a[i] = v*2
+
+print(a)
---
+++ Programming 3/Part2.py
@@ -0,0 +1,16 @@
+inventory = {"Dagger": 0, "BluePotions": 0, "GoldenFeathers": 0, "Manuscrpits": 0}
+abilities = {"StealthPoints": 0, "FlyPoints": 0}
+while True:
+    actionOne = input("You walk through a forest and see a BluePotion, do you pick it up? Y/N: ")
+    if actionOne == "y":
+        inventory['BluePotions']+=1
+        print("Your BluePotions are at least this much: ", inventory['BluePotions'])
+        break
+    if actionOne == "n":
+        print("You dummy...")
+        break
+    if actionOne == ' ':
+        break
+while True:
+    actionTwo = input("You continue walking and find 2 GoldenFeathers, do you pick them up? Y/N: ")
+    if 
